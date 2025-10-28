e55e073929e32c142b3c3c751071a95abc244e5d
Janith Bodaragama <janithofficial12@gmail.com>
2025-10-28 11:11:36 +0530

    updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,8 @@
-import { Stack } from 'expo-router'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { ErrorBoundary } from '@/components'
-import { useEffect } from 'react'
+import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import '../global.css'
 
 // Keep the splash screen visible while we fetch resources
@@ -22,23 +22,21 @@ const RootLayout = (): React.JSX.Element => {
     <SafeAreaProvider>
       <ErrorBoundary>
         <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen 
-            name="splash" 
-            options={{ 
+          <Stack.Screen
+            name="splash"
+            options={{
               headerShown: false,
               statusBarStyle: 'dark',
-              statusBarTranslucent: true,
               navigationBarHidden: true,
-            }} 
+            }}
           />
-          <Stack.Screen 
-            name="user-selection" 
-            options={{ 
+          <Stack.Screen
+            name="user-selection"
+            options={{
               headerShown: false,
               statusBarStyle: 'dark',
-              statusBarTranslucent: true,
               navigationBarHidden: true,
-            }} 
+            }}
           />
           <Stack.Screen name="login" />
           <Stack.Screen name="(tabs)" />
